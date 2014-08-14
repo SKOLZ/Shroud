7a2c3059eb7783f537cd0a7f835622050c67772c
Kaz <inakilanusse@gmail.com>
2014-08-14 01:47:51 -0300

    Initial commit

---
+++ Assets/Scripts/EnemyMobility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMobility : MonoBehaviour {
+
+	public float speed;
+	public GameObject player;
+
+	void FixedUpdate() {
+		float z = Mathf.Atan2 ((player.gameObject.transform.position.y - transform.position.y), (player.gameObject.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+		transform.eulerAngles = new Vector3 (0, 0, z);
+		rigidbody2D.AddForce (gameObject.transform.up * speed);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) 
+	{
+		if (col.gameObject == player) {
+			Debug.Log("asd");	
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerMobility : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate() {
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+		transform.rotation = rot;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		rigidbody2D.angularVelocity = 0;
+
+		/*float input = Input.GetAxis ("Vertical");
+		rigidbody2D.AddForce (gameObject.transform.up * speed * input);*/
+
+		if (Input.GetKey (KeyCode.W)) {
+			var dif = (mousePosition - transform.position);
+			if (Math.Abs(dif.x) > 3 || Math.Abs(dif.y) > 3){
+				rigidbody2D.AddForce (gameObject.transform.up * speed);
+			}
+		}	
+		if(Input.GetKey(KeyCode.A)) {
+			rigidbody2D.AddForce(-gameObject.transform.right * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.S)) {
+			rigidbody2D.AddForce(-gameObject.transform.up * 0.7f * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.D)) {
+			rigidbody2D.AddForce(gameObject.transform.right * speed);
+		}
+	}
+}
