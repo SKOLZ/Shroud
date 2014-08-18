e0417a96574c8d1f06df5a0749a82054e436858b
Kaz <inakilanusse@gmail.com>
2014-08-18 20:17:38 -0300

    Zombie sounds

---
+++ Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSounds : MonoBehaviour {
+
+	public AudioClip[] sounds;
+
+	protected void playSound() {
+		if (audio.isPlaying) return; // don't play a new sound while the last hasn't finished
+		audio.clip = sounds[Random.Range(0,sounds.Length)];
+		audio.Play();
+	}
+}
---
+++ Assets/Scripts/ZombieSounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ZombieSounds : RandomSounds {
+
+	public GameObject player;
+	public float soundRadius = 2.0f;
+	// Update is called once per frame
+	void Update () {
+		if(closeToPlayer())
+			playSound();
+	}
+
+	bool closeToPlayer() {
+		var dif = (transform.position - player.transform.position );
+		return (Math.Abs(dif.x) < soundRadius && Math.Abs(dif.y) < soundRadius);
+	}
+}
