6ebb50f19675267d86b5a26b09560dc79c6a6423
Kaz <inakilanusse@gmail.com>
2014-08-26 01:05:40 -0300

    Main Menu working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private Rect play = new Rect(220, 250, 100, 40);
+	private Rect about = new Rect(220, 320, 100, 40);
+	private Rect exit = new Rect(220, 390, 100, 40);
+
+	public GUIStyle playStyle;
+	public GUIStyle aboutStyle;
+	public GUIStyle exitStyle;
+	public GUIText aboutText;
+
+	void Start() {
+		aboutText.enabled = false;
+		aboutText.text = "Developers: \n\tGabriel Zanzotti\n\tIñaki Lanusse\n\nInstructions:\n\tWASD to move towards mouse pointer. \n Survive the night.";
+	}
+
+	void OnGUI() {
+		if(GUI.Button(play, "", playStyle)) {
+			Application.LoadLevel(1);
+		} 
+		if(GUI.Button(about, "", aboutStyle)) {
+			aboutText.enabled = !aboutText.enabled;
+		} 
+		if(GUI.Button(exit, "", exitStyle)) {
+			Application.Quit();
+		}
+	}
+}
