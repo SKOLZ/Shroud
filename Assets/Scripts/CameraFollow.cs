7575829d24d00adfee3c1fc5cbd65714494659eb
SKOLZ <gabriel.zanzotti@gmail.com>
2014-08-27 03:25:57 -0300

    [master] added finisher manager, also implemented the fixed camera.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,25 +8,17 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	public GameObject gameOverPopup;
 	
-	private float minX;
-	private float maxX;
-	private float minY;
-	private float maxY;
-	private float mapX = 100.0f;
-	private float mapY = 100.0f;
+	private float vertExtent;
+	private float horzExtent;
+
+	public float mapEastLimit;
+	public float mapWestLimit;
+	public float mapNorthLimit;
+	public float mapSouthLimit;
 	
 	void Start() {
 		var vertExtent = Camera.main.camera.orthographicSize;   
 		var horzExtent = vertExtent * Screen.width / Screen.height;
-		minX = horzExtent - mapX / 2.0f;
-		maxX = mapX / 2.0f - horzExtent;
-		minY = vertExtent - mapY / 2.0f;
-		maxY = mapY / 2.0f - vertExtent;
-		Debug.Log(minX);
-		Debug.Log(maxX);
-		Debug.Log(minY);
-		Debug.Log(maxY);
-		Debug.Log(camera.WorldToViewportPoint(target.position));
 	}
 
 	void Update () {
@@ -34,7 +26,24 @@ public class CameraFollow : MonoBehaviour {
 			Vector3 point = camera.WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
 			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			bool correctY = correctMovementY(destination);
+			bool correctX = correctMovementX(destination);
+			if(correctY) {
+				if(correctX)	
+					transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);	
+				else {
+					transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, destination.y, destination.z), ref velocity, dampTime);
+				}
+			} else if (correctX)
+				transform.position = Vector3.SmoothDamp(transform.position, new Vector3(destination.x, transform.position.y, destination.z), ref velocity, dampTime);
 		}
+	}
+
+	private bool correctMovementY(Vector3 point) {
+		return (point.y + vertExtent < mapNorthLimit) && (point.y - vertExtent > mapSouthLimit);
+	}
+	
+	private bool correctMovementX(Vector3 point) {
+		return (point.x + horzExtent < mapEastLimit) && (point.x - horzExtent > mapWestLimit);
 	}
 }
---
+++ Assets/Scripts/FinisherManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinisherManager : MonoBehaviour {
+
+	public ClockManager cm;
+
+	void Start () {
+	
+	}
+	
+	void OnCollitionEnter2D (Collider2D col) {
+		if(cm.getHour() > 8) {
+			Application.LoadLevel(3);
+		} else {
+			//play audio 
+		}
+	}
+}
