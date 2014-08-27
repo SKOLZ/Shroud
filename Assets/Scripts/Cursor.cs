9df4edd30f60af6ae7c81e4bcdbbe5c2e9d1f0bc
Kaz <inakilanusse@gmail.com>
2014-08-27 03:19:19 -0300

    Added zombie sounds and cursor

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	public Texture2D cursorImage;
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+
+	void OnGUI() {
+		Vector3 mousePos = Input.mousePosition;
+		Rect pos = new Rect(mousePos.x,Screen.height - mousePos.y,cursorImage.width,cursorImage.height);
+		GUI.Label(pos,cursorImage);
+	}
+}
